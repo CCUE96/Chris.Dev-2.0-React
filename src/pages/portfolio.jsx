2e3f7984525622b91a4afaf65d07c59b7e743b11
Christopher Cuellar <rival100x@gmail.com>
2024-07-18 18:02:50 -0400

    all projects now being transported individually with styling directly from the component

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,13 +1,53 @@
+import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Projects from "../components/Projects";
 
+import IndieIgnite from "../assets/images/IndieIgnite.png";
+import BeforeTheStorm from "../assets/images/BeforeTheStorm.png";
+import DebateCafe from "../assets/images/DebateCafe.png";
+import MyTime from "../assets/images/MyTime.png";
+import QuizBoi from "../assets/images/QuizBoi.png";
+import AniName from "../assets/images/AniName.png";
+
 const Portfolio = () => {
+  const projectsData = [
+    {
+      title: "Debate Cafe",
+      image: DebateCafe,
+      link: "https://github.com/CCUE96/Debate-Cafe",
+    },
+    {
+      title: "Indie Ignite",
+      image: IndieIgnite,
+      link: "https://github.com/SpencerKlink/Indie-Ignite",
+    },
+    {
+      title: "AniName",
+      image: AniName,
+      link: "https://github.com/mikef13x/project-1-null",
+    },
+    {
+      title: "Before The Storm",
+      image: BeforeTheStorm,
+      link: "https://github.com/CCUE96/Before-The-Storm",
+    },
+    {
+      title: "My Time",
+      image: MyTime,
+      link: "https://github.com/CCUE96/My-Day",
+    },
+    {
+      title: "Quiz Boi 2.0",
+      image: QuizBoi,
+      link: "https://github.com/CCUE96/Quiz-Boi-2.0",
+    },
+  ];
+
   return (
     <>
       <Header />
-      <Projects />
-
+      <Projects projectsData={projectsData} />
       <Footer />
     </>
   );
